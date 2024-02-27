01c1dcbbe8f658d6bed8e313980f35f4cfcfa794
Peter Huynh <peterhyh@yahoo.com>
2024-02-27 17:17:23 -0600

    project images added

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from tkinter import *
 
 window = Tk()
 window.title("Flashcards")
-
+window.config(padx=20, pady=20)
+card_front = PhotoImage("./image/card_front.png")
+canvas = Canvas(width=800, height=526)
+canvas.create_image(400, 263, image=card_front)
+canvas.pack()
 
 window.mainloop()
